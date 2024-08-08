771a437cd4de0832155cd22f9fdede712108cd30
trofimov_gk <tro37295@gmail.com>
2024-08-08 17:06:02 +0500

    remont env

---
+++ app/bookings/router.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+from sqlalchemy import select
+
+from app.database import async_session_maker
+from app.bookings.models import Bookings
+
+
+router = APIRouter(
+    prefix="/bookings",
+    tags=["Бронирование"],
+)
+
+
+@router.get("")
+async def get_bookings():
+    async with async_session_maker() as session:
+        query = select(Bookings)
+        result = await session.execute(query)
+        print(result)
---
+++ app/config.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+from pydantic import BaseSettings
+import os
+
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env"))
+
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+
+class Config:
+    env_file = ".env"
+
+
+settings = Settings()
