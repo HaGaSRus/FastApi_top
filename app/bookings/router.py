1821622b2cdca8eed1cd55679b5a7e6598b950d4
trofimov_gk <tro37295@gmail.com>
2024-08-09 16:35:15 +0500

    add jwterror

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from app.bookings.dao import BookingDAO
 from app.bookings.schemas import SBooking
+from app.users.models import Users
+from app.users.dependencies import get_current_user
 
 router = APIRouter(
     prefix="/bookings",
@@ -9,5 +11,5 @@ router = APIRouter(
 
 
 @router.get("")
-async def get_bookings(user):  # -> list[SBooking]:
+async def get_bookings(user: Users = Depends(get_current_user)):  # -> list[SBooking]:
     return await BookingDAO.find_all()
--- app/users/dependencies.py
+++ app/users/dependencies.py
@@ -1,6 +1,16 @@
+from fastapi import Request, HTTPException, status, Depends
+from jose import jwt, JWTError
+
+def get_token(request: Request):
+    token = request.cookies.get("booking_access_token")
+    if not token:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    return token
 
 
+def get_current_user(token: str = Depends(get_token)):
+    try:
 
-
-def get_current_user():
-    return 'user'
+    except JWTError:
+        HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    return "user"
