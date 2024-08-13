8a94a3aeb0b3476973520024ad77d94167e6c656
trofimov_gk <tro37295@gmail.com>
2024-08-13 09:50:21 +0500

    add rooms.dao

--- app/hotels/rooms/dao.py
+++ app/hotels/rooms/dao.py
@@ -5,34 +5,29 @@ from sqlalchemy import and_, func, or_, select
 from app.bookings.models import Bookings
 from app.dao.base import BaseDAO
 from app.database import async_session_maker, engine
-from app.hotels.models import Hotels
 from app.hotels.rooms.models import Rooms
+# from app.logger import logger
 
 
-class HotelDAO(BaseDAO):
-    model = Hotels
+class RoomDAO(BaseDAO):
+    model = Rooms
 
     @classmethod
-    async def find_all(cls, location: str, date_from: date, date_to: date):
+    async def find_all(cls, hotel_id: int, date_from: date, date_to: date):
         """
-               WITH booked_rooms AS (
-                   SELECT room_id, COUNT(room_id) AS rooms_booked
-                   FROM bookings
-                   WHERE
-                       (date_from >= '2023-05-15' AND date_from <= '2023-06-20') OR
-                       (date_from <= '2023-05-15' AND date_to > '2023-05-15')
-                   GROUP BY room_id
-               ),
-               booked_hotels AS (
-                   SELECT hotel_id, SUM(rooms.quantity - COALESCE(rooms_booked, 0)) AS rooms_left
-                   FROM rooms
-                   LEFT JOIN booked_rooms ON booked_rooms.room_id = rooms.id
-                   GROUP BY hotel_id
-               )
-               SELECT * FROM hotels
-               LEFT JOIN booked_hotels ON booked_hotels.hotel_id = hotels.id
-               WHERE rooms_left > 0 AND location LIKE '%Алтай%';
-               """
+        WITH booked_rooms AS (
+            SELECT room_id, COUNT(room_id) AS rooms_booked
+            FROM bookings
+            WHERE (date_from >= '2023-05-15' AND date_from <= '2023-06-20') OR
+                  (date_from <= '2023-05-15' AND date_to > '2023-05-15')
+            GROUP BY room_id
+        )
+        SELECT
+            -- все столбцы из rooms,
+            (quantity - COALESCE(rooms_booked, 0)) AS rooms_left FROM rooms
+        LEFT JOIN booked_rooms ON booked_rooms.room_id = rooms.id
+        WHERE hotel_id = 1
+        """
         booked_rooms = (
             select(Bookings.room_id, func.count(Bookings.room_id).label("rooms_booked"))
             .select_from(Bookings)
@@ -52,31 +47,18 @@ class HotelDAO(BaseDAO):
             .cte("booked_rooms")
         )
 
-        booked_rooms = (
-            select(Rooms.hotel, func.sum(
-                Rooms.quantity - func.coalesce(booked_rooms.c.rooms_booked, 0)
-            ).label("rooms_left"))
-            .select_from(Rooms)
-            .join(booked_rooms, booked_rooms.c.room_id == Rooms.id, isouter=True)
-            .group_by(Rooms.hotel_id)
-            .cte("booked_hotels")
-        )
-
-        get_hotels_with_rooms = (
+        get_rooms = (
             select(
-                Hotels.__table__.columns,
-                booked_hotels.c.rooms_letf,
+                Rooms.__table__.columns,
+                (Rooms.price * (date_to - date_from).days).label("total_cost"),
+                (Rooms.quantity - func.coalesce(booked_rooms.c.rooms_booked, 0)).label("rooms_left"),
             )
-            .join(booked_hotels, booked_hotels.c.hotel_id == Hotels.id, isouter=True)
+            .join(booked_rooms, booked_rooms.c.room_id == Rooms.id, isouter=True)
             .where(
-                and_(
-                    booked_hotels.c.rooms_left > 0,
-                    Hotels.location.like(f"%{location}"),
-                )
+                Rooms.hotel_id == hotel_id
             )
         )
         async with async_session_maker() as session:
-            # logger.debug(get_hotels_with_rooms.compile(engine, compile_kwargs={"literal_binds": True}))
-            hotels_with_rooms = await session.execute(get_hotels_with_rooms)
-            return hotels_with_rooms.mapping().all()
-
+            # logger.debug(get_rooms.compile(engine, compile_kwargs={"literal_binds": True}))
+            rooms = await session.execute(get_rooms)
+            return rooms.mappings().all()
