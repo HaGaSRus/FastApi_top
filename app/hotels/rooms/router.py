d3e5ffdec1fe2a6f8ae1b625df034d7596e22ab1
trofimov_gk <tro37295@gmail.com>
2024-08-13 09:48:17 +0500

    add rooms.router

--- app/hotels/rooms/router.py
+++ app/hotels/rooms/router.py
@@ -1,6 +1,17 @@
 from datetime import date, timedelta, datetime
-from typing import list
+from typing import List
 
 from fastapi import Query
 
 from app.hotels.rooms.dao import RoomsDAO
+from app.hotels.rooms.schemas import SRoomInfo
+from app.hotels.router import router
+
+@router.get("/{hotel_id}/rooms")
+async def get_rooms_by_time(
+        hotel_id: int,
+        date_from: date = Query(..., description=f"Например, {datetime.now().date()}"),
+        date_to: date = Query(..., description=f"Например, {(datetime.now() + timedelta(days=14)).date()}"),
+) -> List[SRoomInfo]:
+    rooms = await RoomsDAO.find_all(hotel_id, date_from, date_to)
+    return rooms
