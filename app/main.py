eb94d6a9c32c99f720b32d68c66977f98cf86345
trofimov_gk <tro37295@gmail.com>
2024-08-14 11:25:03 +0500

    Rebuild

--- app/main.py
+++ app/main.py
@@ -3,25 +3,16 @@ from fastapi import FastAPI, Query, Depends
 from fastapi.staticfiles import StaticFiles
 from starlette.middleware.cors import CORSMiddleware
 
-from app.bookings.router import router as router_bookings
-from app.hotels.router import router as router_hotels
+
 from app.users.router import router_auth, router_users
 
-from app.pages.router import router as router_pages
-from app.images.router import router as router_images
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="app/static"), "static")
-
 
 app.include_router(router_users)
-app.include_router(router_bookings)
-app.include_router(router_hotels)
 app.include_router(router_auth)
 
-app.include_router(router_pages)
-app.include_router(router_images)
 
 origins = ["*"]
 
