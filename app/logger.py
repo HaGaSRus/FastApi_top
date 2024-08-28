fa63db4d8c2b6d8c0ca385b3ecb5b6351161f7be
trofimov_gk <tro37295@gmail.com>
2024-08-28 13:44:26 +0500

    Update logger

--- app/logger.py
+++ app/logger.py
@@ -1,33 +1,28 @@
+from pythonjsonlogger import jsonlogger
 import logging
 from datetime import datetime
-from typing import Dict, Any
-
-from pythonjsonlogger import jsonlogger
-
 from app.config import settings
-
-logger = logging.getLogger()
-
-logHandler = logging.StreamHandler()
-
 
 class CustomJsonFormatter(jsonlogger.JsonFormatter):
     def add_fields(self, log_record, record, message_dict):
         super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
         if not log_record.get("timestamp"):
-            now = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+            now = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")  # Измененный формат времени
             log_record["timestamp"] = now
         if log_record.get("level"):
             log_record["level"] = log_record["level"].upper()
         else:
             log_record["level"] = record.levelname
 
-
+# Настроить форматтер для логирования
 formatter = CustomJsonFormatter(
     "%(timestamp)s %(level)s %(message)s %(module)s %(funcName)s"
 )
 
+# Настроить обработчик и логгер
+logHandler = logging.StreamHandler()
 logHandler.setFormatter(formatter)
+
+logger = logging.getLogger()
 logger.addHandler(logHandler)
 logger.setLevel(settings.LOG_LEVEL)
-
--- app/users/auth.py
+++ app/users/auth.py
@@ -22,7 +22,7 @@ def verify_password(plain_password, hashed_password) -> bool:
 
 def create_access_token(data: dict) -> str:
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=30)
+    expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(
         to_encode,
