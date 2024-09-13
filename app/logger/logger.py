36a18e1c53f1e496328782eebac112a275d00607
trofimov_gk <tro37295@gmail.com>
2024-09-13 10:43:21 +0500

    logger update

--- app/logger/logger.py
+++ app/logger/logger.py
@@ -1,33 +1,44 @@
-import logging
 from pythonjsonlogger import jsonlogger
-
+import logging
+from datetime import datetime
+import pytz
+from app.config import settings
+import os
 
 class CustomJsonFormatter(jsonlogger.JsonFormatter):
     def __init__(self, *args, **kwargs):
-        # Инициализация форматера с ключами JSON
-        super().__init__(*args, **kwargs)
+        kwargs['json_ensure_ascii'] = False
+        super(CustomJsonFormatter, self).__init__(*args, **kwargs)
 
     def add_fields(self, log_record, record, message_dict):
-        super().add_fields(log_record, record, message_dict)
-        if not log_record.get('timestamp'):
-            log_record['timestamp'] = self.formatTime(record, self.datefmt)  # Устанавливаем временную метку
-        if not log_record.get('level'):
-            log_record['level'] = record.levelname  # Устанавливаем уровень логирования
+        super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
+        if not log_record.get("timestamp"):
+            yekaterinburg_tz = pytz.timezone('Asia/Yekaterinburg')
+            now = datetime.now(yekaterinburg_tz)
+            formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
+            log_record["timestamp"] = formatted_time
+        if log_record.get("level"):
+            log_record["level"] = log_record["level"].upper()
+        else:
+            log_record["level"] = record.levelname
 
-# Конфигурация логгера
+# Настройте форматировщик для ведения журнала с помощью json_ensure_ascii=False
+formatter = CustomJsonFormatter(
+    "%(timestamp)s %(level)s %(message)s %(module)s %(funcName)s"
+)
+
+# Настраиваем обработчики и логгер
+logHandler = logging.StreamHandler()
+logHandler.setFormatter(formatter)
+
+# Определяем путь к файлу лога в той же директории, где находится основной скрипт
+log_file_path = os.path.join(os.path.dirname(__file__), 'app_logs.json')
+
+# Настраиваем обработчик для записи в файл
+fileHandler = logging.FileHandler(log_file_path)
+fileHandler.setFormatter(formatter)
+
 logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-
-# Настройка JSON форматтера
-json_formatter = CustomJsonFormatter('%(timestamp)s %(level)s %(module)s %(funcName)s %(message)s')
-
-# Создание и настройка обработчика
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(json_formatter)
-
-# Добавляем обработчик к логгеру
-logger.addHandler(console_handler)
-
-# Пример логирования
-logger.info("This is an info log")
-logger.error("This is an error log")
+logger.addHandler(logHandler)
+logger.addHandler(fileHandler)
+logger.setLevel(settings.LOG_LEVEL)
