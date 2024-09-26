e78d1bf48fde274fb58d211e5205c182afde91f8
trofimov_gk <tro37295@gmail.com>
2024-09-26 16:58:51 +0500

    Двигаемся к depth

--- app/questions/models.py
+++ app/questions/models.py
@@ -69,7 +69,7 @@ class SubQuestion(Base):
     number = Column(Integer, nullable=True)
 
     # Связь с родительским подвопросом (если есть)
-    parent_subquestion_id = Column(Integer, ForeignKey('sub_questions.id', name='fk_subquestions_parent_subquestion_id'), nullable=True)
+    # parent_subquestion_id = Column(Integer, ForeignKey('sub_questions.id', name='fk_subquestions_parent_subquestion_id'), nullable=True)
 
     # Обратная связь к вопросу
     question = relationship("Question", back_populates="sub_questions")
